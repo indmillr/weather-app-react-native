50efb9fc8c00071193e7337aa442dce0dd3bb223
Ian Miller <theiandmiller@gmail.com>
2022-10-13 05:55:28 -0500

    project init

--- App.js
+++ App.js
@@ -1,10 +1,34 @@
 import { StatusBar } from "expo-status-bar";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
 
 export default function App() {
+   // define state for Error Message related to Location permissions
+   const [errorMessage, setErrorMessage] = useState(null);
+
+   // location permissions on Load
+   useEffect(() => {
+      load();
+   }, []);
+
+   // a function to make sure on load that access to Location is permitted
+   async function load() {
+      try {
+         let { status } = await Location.requestBackgroundPermissionsAsync();
+         if (status !== "granted") {
+            setErrorMessage("Access to Location is needed to run the App");
+            return;
+         }
+         const location = await Location.getCurrentPositionAsync();
+         const { latitude, longitude } = location.coords;
+         alert(`Latitude: ${latitude}, Longitude: ${longitude}`);
+      } catch (error) {}
+   }
+
    return (
       <View style={styles.container}>
-         <Text>Hello from React Native!</Text>
+         <Text>Hello from ReactNative!</Text>
          <StatusBar style="auto" />
       </View>
    );
@@ -13,7 +37,7 @@ export default function App() {
 const styles = StyleSheet.create({
    container: {
       flex: 1,
-      backgroundColor: "#fff",
+      backgroundColor: "red",
       alignItems: "center",
       justifyContent: "center",
    },
